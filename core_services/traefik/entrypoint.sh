815382a8d3a0e8c54e6fc20607d414468af3049d
pcs3rd <rhean1620@gmail.com>
2026-04-05 09:20:54 -0400

    One more try

--- core_services/traefik/entrypoint.sh
+++ core_services/traefik/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Write the bouncer key from env to a tmpfs file for the plugin to read
-echo -n "$BOUNCER_API_KEY" > /run/secrets/bouncer_api_key
+echo -n "$BOUNCER_KEY_TRAEFIK" > /run/secrets/bouncer_api_key
 exec /entrypoint.sh "$@"
